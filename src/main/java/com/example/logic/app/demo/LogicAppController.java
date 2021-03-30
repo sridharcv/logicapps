f44301909317eecbaa8c30112904d615a77ea2fd
sridhar chipuri venkata <sridharchipurivenkata@sridhars-MBP.attlocal.net>
2021-03-30 17:14:33 -0400

    update code1

--- src/main/java/com/example/logic/app/demo/LogicAppController.java
+++ src/main/java/com/example/logic/app/demo/LogicAppController.java
@@ -2,8 +2,9 @@ package com.example.logic.app.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class LogicAppController {
 	
 	
-	private  static String url="none";
+	private  static String url="none1";
 	
 	@PostMapping("/entryPoint")
-	public String entryPoint(@RequestParam(name = "callBackUrl") TestMethod url) {
+	public String entryPoint(@RequestBody String callBackUrl) {
 		System.out.println("in the host");
-		this.url=url.getUrl();
+		this.url=callBackUrl;
 		return "hello";
 	}
 	
